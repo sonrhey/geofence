7fc9f4deabfdf20c5b753f85c08774ece8e832c5
Son Rhey Deiparine <sonrheydeiparine2@gmail.com>
2020-07-20 03:07:33 +0800

    updated git ignore

--- www/js/custom_js/config.js
+++ www/js/custom_js/config.js
@@ -3,6 +3,6 @@ var load_navigation = $(function(){
 });
 
 function ipaddress(){
-    return 'http://192.168.43.254/geofence/app_processes/';
+    return 'http://192.168.43.254/geofence/app_processes/s';
     // return 'http://127.0.0.1/ettendance/app_processes/';
 }
